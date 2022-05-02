1a2fb622c1098af3a9768177a78b84255ac57768
Foo-x <www.sand.x@gmail.com>
2022-05-02 22:06:03 +0900

    feat: remove home title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Seo from '../components/seo';
 
 const IndexPage: React.FC<PageProps> = () => (
   <Layout>
-    <Seo title='Home' />
+    <Seo />
     <NavTab selectedCategory='map' />
   </Layout>
 );
